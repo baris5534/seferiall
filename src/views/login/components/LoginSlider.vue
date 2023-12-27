f182b91f312cc0acb3159e94a09ddde855819b53
baris5534 <samsuncrsmb55@gmail.com>
2023-12-27 17:30:31 +0300

    27aralik17:30

---
+++ src/views/login/components/LoginSlider.vue
@@ -0,0 +1,192 @@
+<template>
+  <carousel :per-page="1" :navigate-to="someLocalProperty" :mouse-drag="true" :loop="true" :autoplay="true"
+    :autoplayTimeout="2500">
+    <slide>
+      <div class="arkaplan1">
+        <div class="p-5 h-screen flex flex-col justify-between w-[480px] lg:w-[260px] xl:w-[320px] 2xl:w-[380px] min-[1920px]:w-[420px]">
+          <div>
+            <img src="@/assets/SIRIUS_TEDARİK_Logo_Full Beyaz-01 1.png" alt="" class="lg:w-3/6 xl:w-2/6 2xl:w-3/6 min-[1920px]:w-4/6"/>
+          </div>
+          <div class="text-left float-end space-y-2 w-full">
+            <div class="pb-20 xl:pb-20 lg:pb-5 space-y-8 flex flex-col w-full">
+              <h4 class="font-black text-3xl lg:text-xl xl:text-2xl 2xl:text-3xl min-[1920px]:text-3xl text-white w-full">
+                Zamanı En İyi Şekilde Kullan
+              </h4>
+              <span class="text-base lg:text-xs xl:text-base 2xl:text-sm min-[1920px]:text-xl text-white">Zamanınızı ve kaynaklarınızı etkin bir şekilde yönetin.
+                Yolculuklarınızı tasarruflu bir şekilde planlayarak, hızlı ve
+                zamanında teslimatlar ile iş süreçlerinizi optimize edin.</span>
+            </div>
+            <div class="flex space-x-8 lg:space-x-4 items-center py-4 lg:py-2">
+              <span><img src="@/assets/simple-icons_x.png" alt="" class="lg:w-4/6"/></span>
+              <span><img src="@/assets/ri_instagram-fill.png" alt="" class="lg:w-4/6"/></span>
+              <span><img src="@/assets/mdi_linkedin.png" alt="" class="lg:w-4/6"/></span>
+            </div>
+            
+            <div>
+              <span class="text-white lg:text-xs xl:text-sm min-[1920px]:text-lg">Lorem ipsum dolor sit amet consectetur.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </slide>
+    <slide>
+      <div class="arkaplan2">
+        <div class="p-5 h-screen flex flex-col justify-between w-[480px] lg:w-[260px] xl:w-[320px] 2xl:w-[380px] min-[1920px]:w-[420px]">
+          <div>
+            <img src="@/assets/SIRIUS_TEDARİK_Logo_Full Beyaz-01 1.png" alt="" class="lg:w-3/6 xl:w-3/6 2xl:w-3/6 min-[1920px]:w-4/6"/>
+          </div>
+          <div class="text-left float-end space-y-2">
+            <div class="pb-20 xl:pb-20 lg:pb-5 space-y-8 flex flex-col">
+              <h4 class="font-black lg:text-xl text-3xl xl:text-2xl 2xl:text-3xl min-[1920px]:text-3xl text-white">
+                Yakıt Tüketimi Tasarrufunda Öncü
+              </h4>
+              <span class="text-base lg:text-xs xl:text-base 2xl:text-sm min-[1920px]:text-xl text-white">Yenilikçi teknolojimiz sayesinde, yakıt tüketimini optimize
+                ederek çevre dostu ve ekonomik bir lojistik deneyimi sunuyoruz.
+                İleri seviye verimlilikle, tasarrufun keyfini çıkarın.</span>
+            </div>
+            <div class="flex space-x-8 lg:space-x-4 items-center py-4 lg:py-2">
+              <span><img src="@/assets/simple-icons_x.png" alt="" class="lg:w-4/6"/></span>
+              <span><img src="@/assets/ri_instagram-fill.png" alt="" class="lg:w-4/6"/></span>
+              <span><img src="@/assets/mdi_linkedin.png" alt="" class="lg:w-4/6"/></span>
+            </div>
+            
+            <div>
+              <span class="text-white lg:text-xs xl:text-sm min-[1920px]:text-lg">Lorem ipsum dolor sit amet consectetur.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </slide>
+    <slide>
+      <div class="arkaplan3">
+        <div class="p-5 h-screen flex flex-col justify-between w-[480px] lg:w-[260px] xl:w-[320px] 2xl:w-[380px] min-[1920px]:w-[420px]">
+          <div>
+            <img src="@/assets/SIRIUS_TEDARİK_Logo_Full Beyaz-01 1.png" alt="" class="lg:w-3/6 xl:w-3/6 2xl:w-3/6 min-[1920px]:w-4/6"/>
+          </div>
+          <div class="text-left float-end space-y-2 ">
+            <div class="pb-20 xl:pb-20 lg:pb-5 space-y-8 flex flex-col">
+              <h4 class="font-black lg:text-xl text-3xl xl:text-2xl 2xl:text-3xl min-[1920px]:text-3xl text-white">
+                Araç Takibiyle Kontrol Senin Elinde
+              </h4>
+              <span class="text-base lg:text-xs xl:text-base 2xl:text-sm min-[1920px]:text-xl text-white">Araçlarınızın gerçek zamanlı takibiyle güvenliği artırın,
+                teslimat süreçlerini hızlandırın ve operasyonel verimliliği en
+                üst düzeye çıkarın. Kontrol sizin elinizde.</span>
+            </div>
+            <div class="flex space-x-8 lg:space-x-4 items-center py-4 lg:py-2">
+              <span><img src="@/assets/simple-icons_x.png" alt="" class="lg:w-4/6"/></span>
+              <span><img src="@/assets/ri_instagram-fill.png" alt="" class="lg:w-4/6"/></span>
+              <span><img src="@/assets/mdi_linkedin.png" alt="" class="lg:w-4/6"/></span>
+            </div>
+            
+            <div>
+              <span class="text-white lg:text-xs xl:text-sm min-[1920px]:text-lg">Lorem ipsum dolor sit amet consectetur.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </slide>
+  </carousel>
+  
+</template>
+
+<script>
+
+export default {
+  components: {
+ }
+};
+</script>
+  
+<style>
+.arkaplan1 {
+  background-image: url("@/assets/carousel1.png");
+}
+
+.arkaplan2 {
+  background-image: url("@/assets/Rectangle 2.png");
+}
+
+.arkaplan3 {
+  background-image: url("@/assets/Rectangle 10.png");
+}
+
+.içi-beyaz-çizgi {
+  border-left-width: 70px;
+  height: 7px;
+  background-color: white;
+  border-radius: 20px;
+}
+.VueCarousel-dot{
+  width: 30px!important;
+  height: 7px!important;
+  border-radius: 20px!important;
+}
+.VueCarousel-pagination{
+  position: absolute;
+  top: 80%;
+  padding: 0 10px;
+}
+.VueCarousel-dot-container{
+  float:left;
+}
+
+.VueCarousel-dot{
+  background-color: white!important;
+  opacity: 50%!important;
+}
+.VueCarousel-dot--active{
+  background-color: #fff!important;
+  opacity: 100%!important;
+}
+.VueCarousel-dot[data-v-438fd353]:focus {
+    outline: 0px solid lightblue!important;
+}
+@media only screen and (max-width: 1920px) {
+  .VueCarousel-dot{
+  width: 50px!important;
+  height: 7px!important;
+  border-radius: 20px!important;
+}
+  .VueCarousel-pagination{
+  position: absolute;
+  top: 85%;
+  padding: 0 10px;
+}
+}
+@media only screen and (max-width: 1280px){
+  .VueCarousel-dot{
+  width: 50px!important;
+  height: 7px!important;
+  border-radius: 20px!important;
+}
+.VueCarousel-pagination{
+  position: absolute;
+  top: 80%;
+  padding: 0 10px;
+}
+}
+@media only screen and (max-width: 1024px){
+  .VueCarousel-dot{
+  width: 30px!important;
+  height: 7px!important;
+  border-radius: 20px!important;
+}
+.VueCarousel-pagination{
+  position: absolute;
+  top: 75%;
+  padding: 0 10px;
+}
+}
+@media only screen and (max-width: 1536px){
+  .VueCarousel-dot{
+  width: 50px!important;
+  height: 7px!important;
+  border-radius: 20px!important;
+}
+.VueCarousel-pagination{
+  position: absolute;
+  top: 73%;
+  padding: 0 10px;
+}
+}
+</style>
---
+++ src/views/login/İndex.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <div class="grid grid-cols-12">
+      <div class="col-span-3 bg-blue-700  h-screen "> 
+        <LoginSlider></LoginSlider>
+      </div>
+      <div class="col-span-9 flex items-center">
+        <div class="flex flex-col justify-between h-5/6 items-center mx-auto">
+          <div class="flex flex-col items-center">  
+            <img src="@/assets/SEFERİ_Logo1.png" alt="" class="w-4/6 xl:w-4/6 lg:w-3/6 min-[1920px]:w-5/6">
+          </div>
+          <div class="flex flex-col">
+            <h2 class="text-3xl min-[1920px]:text-4xl xl:text-3xl lg:text-2xl text-[#0066B1] font-black">Lojistikte Yenilik Başlasın!</h2>
+            <span class="text-base min-[1920px]:text-xl xl:text-base lg:text-sm text-[#A8A8A8]">Lorem ipsum dolor sit amet consectetur.</span>
+          </div>
+          <div class="flex flex-col w-2/5">
+            <h4 class="text-2xl min-[1920px]:text-3xl xl:text-2xl lg:text-xl text-[#0066B1] py-4 font-bold">Giriş</h4>
+            <div class="flex flex-col text-left py-3">
+              <span class="text-base min-[1920px]:text-xl xl:text-base lg:text-sm text-[#A8A8A8] py-2">Kullanıcı Ad</span>
+              <input type="text" name="" id="" class="border-[#0066B1] bg-[#F6F6F6] border rounded-lg p-2 caret-[#0066B1] focus:outline-none focus:border-[#0066B1] focus:ring-1 focus:ring-[#0066B1]">
+            </div>
+            <div class="flex flex-col text-left">
+              <span class="text-base min-[1920px]:text-xl xl:text-base lg:text-sm text-[#A8A8A8] py-2">Şifre</span>
+              <input type="password" name="" id="" class="border-[#0066B1] bg-[#F6F6F6] border rounded-lg p-2 caret-[#0066B1] focus:outline-none focus:border-[#0066B1] focus:ring-1 focus:ring-[#0066B1]">
+              <div class="flex flex-row justify-between items-center pt-2">
+                <div class="flex items-center">
+                  <input type="checkbox" name="" id="" class="bg-[#D9D9D9]">
+                  <span class="text-[#A8A8A8] text-sm xl:text-sm lg:text-xs min-[1920px]:text-base px-2">Beni Hatırla</span>
+                </div>
+                
+              <span class="text-[#A8A8A8] text-sm min-[1920px]:text-base xl:text-sm lg:text-xs text underline">
+                <router-link to="#">Şifremi unuttum</router-link>
+                
+              </span>
+            </div>
+              
+            </div>
+          
+          <button class="bg-[#0066B1] hover:bg-opacity-90 w-3/3 p-3 rounded-lg my-8">
+            <router-link to="#" class="w-full text-white text-xl min-[1920px]:text-2xl xl:text-xl w-3/3 font-bold lg:text-base">Giriş Yap</router-link>
+          </button>
+        </div>
+            <p class="text-[#A8A8A8] text-base min-[1920px]:text-xl xl:text-base lg:text-sm">Lorem ipsum dolor sit amet consectetur. Et scelerisque massa rutrum nibh varius in purus justo ornare.</p>
+        </div>
+        <div class="bg-blue-700 h-screen">
+            <span class="p-2"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginSlider from './components/LoginSlider.vue' ;
+export default {
+
+  name: 'HomeView',
+  components: {
+    LoginSlider,
+  },
+}
+</script>
+<style scoped>
+
+</style>
